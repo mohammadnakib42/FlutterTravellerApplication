6158f172a637015ea82397b87fb75e9534466b31
mohammadnakib42 <62894567+mohammadnakib42@users.noreply.github.com>
2021-12-14 22:55:37 +0600

    day3 completed

---
+++ lib/pages/navpages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class HoemPage extends StatefulWidget {
+  const HoemPage({ Key? key }) : super(key: key);
+
+  @override
+  _HoemPageState createState() => _HoemPageState();
+}
+
+class _HoemPageState extends State<HoemPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
--- lib/widgets/responsive_button.dart
+++ lib/widgets/responsive_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class ResponsiveButton extends StatelessWidget {
   bool? isResponsive;
   double? width;
