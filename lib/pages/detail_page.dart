d13c3c07b6e4b5aff30ca6ff11e791ba121ac874
mohammadnakib42 <62894567+mohammadnakib42@users.noreply.github.com>
2021-12-20 12:45:23 +0600

    day6 completed

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:traveller/widgets/app_large_text.dart';
+import 'package:traveller/widgets/app_text.dart';
 
 class DetailPage extends StatefulWidget {
-  const DetailPage({ Key? key }) : super(key: key);
+  const DetailPage({Key? key}) : super(key: key);
 
   @override
   _DetailPageState createState() => _DetailPageState();
@@ -13,19 +15,104 @@ class _DetailPageState extends State<DetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        width:double.maxFinite,
+        width: double.maxFinite,
         height: double.maxFinite,
         child: Stack(
           children: [
             Positioned(
+              left: 0,
+              right: 0,
+              child: Container(
+                width: double.maxFinite,
+                height: 350,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        'https://images.pexels.com/photos/1172064/pexels-photo-1172064.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+                    //image: AssetImage("images\levender.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
               left: 20,
-              top: 70,
+              top: 50,
               child: Row(
-              children: [
-                IconButton(onPressed: () {},icon: Icon(Icons.menu),),
-                
-              ],
-            ),),
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.menu),
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              top: 320,
+              child: Container(
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 30,
+                ),
+                width: MediaQuery.of(context).size.width,
+                height: 500,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        AppLargeText(
+                          text: "Kanishail",
+                          color: Colors.black,
+                        ),
+                        AppLargeText(
+                          text: "\$ 250",
+                          color: Colors.purple,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.location_on,
+                          color: Colors.amber,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        AppText(
+                          text: "Bangladesh, Sylhet",
+                          color: Colors.grey,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Wrap(
+                      children: List.generate(
+                        5,
+                        (index) {
+                          return Icon(Icons.stars);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
